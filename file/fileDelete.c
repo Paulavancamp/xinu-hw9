c8b517c6487124f9f81145c21941efc08d8a0b6f
Paulavancamp <pvancamp1@morbius.mscs.mu.edu>
2019-04-15 23:41:34 -0500

    created 3 cases for sbFreeBlock

--- file/fileDelete.c
+++ file/fileDelete.c
@@ -17,12 +17,22 @@
  */
 devcall fileDelete(int fd)
 {
-    // TODO: Unlink this file from the master directory index,
+    //  Unlink this file from the master directory index,
     //  and return its space to the free disk block list.
     //  Use the superblock's locks to guarantee mutually exclusive
     //  access to the directory index.
-	filetab[fd].fn_state = FILE_FREE;
-	//call sbFreeBlock(struct superblock *psuper, int block);
+
+	//enforce mutual exclusion
+	wait(dirlock);
+	//call freeblock using the given location within the list
+	sbFreeBlock(supertab, supertab->dirlist->db_fnodes[fd].blocknum);
+	//set state to free
+	fn_state = FREE;
+	//idk...
+	seek(DISK0, supertab->sb_dirlist->db_blocknum);
+	write(DISK0, supertab->sb_dirlist, sizeof(struct dirblock));
+	//end critical section
+	signal(dirlock);
 
    return OK;
 }
--- file/sbFreeBlock.c
+++ file/sbFreeBlock.c
@@ -3,8 +3,7 @@
 /*                                                                 */
 /* Modified by Alexander Kosla and Paula Van Camp                  */
 /*                                                                 */
-/* TA-BOT:MAILTO paula.vancamp@marquette.edu alexander.kosla@marquette.edu */
-/*                                                                 */
+/*TA-BOT:MAILTO paula.vancamp@marquette.edu alexander.kosla@marquette.edu */
 
 #include <kernel.h>
 #include <device.h>
@@ -18,30 +17,69 @@
  */
 devcall sbFreeBlock(struct superblock *psuper, int block)
 {
-    // TODO: Add the block back into the filesystem's list of free blocks.
-    //  Use the superblock's locks to guarantee mutually exclusive access to the free list, and write the changed free list segment(s) back to disk.
-    struct freeblock *frisbee, *prevblock;
-    if (psuper == NULL){
-	return SYSERR; //no pointer to super
-    }
-    if(block == NULL){
-	return SYSERR;//no block given
+//Add the block back into the filesystem's list of free blocks.
+//Use the superblock's locks to guarantee mutually exclusive access
+//to the free list, and write the changed free list segment(s) back to disk.
+
+    if ((psuper == NULL)||(block == NULL)){
+	return SYSERR;//no block or pointer given
     }
 
+    struct dentry *phw;
+    phw = psuper->sb_disk;
+    if(phw == NULL){
+	return SYSERR;
+    }
+
+    //set up freeblock and others
+    freeblock = (struct freeblock*) malloc(sizeof(struct freeblock));
+    struct freeblock *newblock, *nextBlock;
+    struct dirblock *swizzle;
     //enforce mutual exclusion	
     wait(psuper->sb_freelock);
-   
-    //find poisition in list
-    frisbee.fr_blocknum = block;
-    frisbee.fr_count++;
-    
-    //insert into list of free blocks
-    prevblock.fr_blocknum = block-1;
-    frisbee.fr_next = prevblock.fr_next;
-    prevblock.fr_next = frisbee;
- 
 
-    //release lock
+/************************CRITICAL SECTION*******************************/
+
+    //initialize freeblock to be the freeblock
+    freeblock = psuper->sb_freelist;
+    //CASE 3: if no freeblk
+    if(freeblock == NULL){
+	//create and add to head of sb freelist
+	printf("FREEBLOCK IS NULL \r\n\t");
+	newBlock->fr_count = 0;
+	newBlock->fr_next = NULL;
+	newBlock->fr_blocknum = block;
+	/*swizzle*/
+	swizzle = psuper->sb_dirlst;
+	psuper->sb_dirlst = (struct dirblock *)swizzle->db_blocknum;
+    	seek(diskfd, block);
+	if(write(diskfd, freeblk, sizeof(struct freeblock))){
+	    return SYSERR;
+	}
+	/*restore swizzle*/
+	psuper->sb_dirlst = swizzle;
+	signal(psuper->sb_freelist);
+	return OK;
+    }
+    //**CASE 2: if block is full
+    if(freeblock->fr_count==FREEBLOCKMAX){
+	//add to end of freelist
+	printf("BLOCK FULL\r\n\t");
+	newBlock->fr_count = 0;
+	newBlock->fr_next = NULL;
+	newBlock->fr_blocknum = block;
+	freeblock->fr_next = newBlock;
+	}
+	signal(psuper->sb_freelock);
+	return OK;
+    //**CASE 1: Basic Case  
+    if(freelist->fr_count<MAXBLOCKCOUNT){
+    	freeblock->fr_free[block];
+    	freeblock->fr_count++;
+    }
+
+/***********************END CRITICAL SECTION*************************/
     signal(psuper->sb_freelock);
+
     return SYSERR;
 }
